a67224823028e6df5f62a602c57023273f275f62
Peter Suhm <peter@suhm.dk>
2025-08-04 14:27:18 +0200

    set up react and inertia

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = () => {
+    return <div>Home sweet home</div>;
+};
+
+export default Home;
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
+
+Route::get('/home', function () {
+    return Inertia::render('Home');
+})->name('home');
